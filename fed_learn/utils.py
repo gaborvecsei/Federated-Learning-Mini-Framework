a10a97cf23a63d36a33dde45fa5c3cd0eb37207f
Gábor Vecsei <vecseigabor.x@gmail.com>
2019-08-03 20:09:45 +0200

    del model and clear_session calls switched

--- fed_learn/utils.py
+++ fed_learn/utils.py
@@ -1,7 +1,10 @@
 from keras import backend as K
+import gc
 
 
 def get_rid_of_the_models(model=None):
+    # TODO: somehow this does not free up the GPU memory (after a while you will get OOM) (tested on Windows 10...)
+    K.clear_session()
     if model is not None:
         del model
-    K.clear_session()
+    gc.collect()
