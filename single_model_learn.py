e60fe2a4f99c843efdc8ea5f5ef1196f1f4bec98
Gábor Vecsei <vecseigabor.x@gmail.com>
2019-08-03 20:48:16 +0200

    image minmax scaling added to single model run script

--- single_model_learn.py
+++ single_model_learn.py
@@ -8,5 +8,7 @@ model = fed_learn.create_model((32, 32, 3), 10)
 
 (x_train, y_train), (_, _) = datasets.cifar10.load_data()
 y_train = utils.to_categorical(y_train, len(np.unique(y_train)))
+x_train = x_train.astype(np.float32)
+x_train = x_train / 255.0
 
 model.fit(x_train, y_train, batch_size=32, epochs=20, verbose=1)
