5db9b11ae1137535192ca795c31cab4d7ae461ea
Gábor Vecsei <vecseigabor.x@gmail.com>
2019-08-11 18:03:01 +0200

    non iid data sampling implemented

--- fed_learn/data_sampling.py
+++ fed_learn/data_sampling.py
@@ -1,3 +1,5 @@
+import random
+
 import numpy as np
 
 
@@ -8,5 +10,17 @@ def iid_data_indices(nb_clients: int, data_len: int):
     return chunks
 
 
-def non_iid_data_indices(nb_clients: int, data_len: int):
-    raise NotImplementedError
+def non_iid_data_indices(nb_clients: int, y_true: np.ndarray, nb_shards: int = 200):
+    y_true = y_true.flatten()
+    data_len = len(y_true)
+
+    # Sorting data indices by labels
+    indices = np.arange(data_len)
+    indices = indices[y_true.argsort()]
+
+    shards = np.array_split(indices, nb_shards)
+    random.shuffle(shards)
+    shards_for_users = np.array_split(shards, nb_clients)
+    indices_for_users = [np.hstack(x) for x in shards_for_users]
+
+    return indices_for_users
